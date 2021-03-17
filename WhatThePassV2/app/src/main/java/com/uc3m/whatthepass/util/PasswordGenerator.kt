656e9735617ce0df3345c2a522dae0bb10c2df02
alvaro-gar <100363780@alumnos.uc3m.es>
2021-03-17 15:19:59 +0100

    Generador de contraseñas completamente funcional

--- WhatThePassV2/app/src/main/java/com/uc3m/whatthepass/util/PasswordGenerator.kt
+++ WhatThePassV2/app/src/main/java/com/uc3m/whatthepass/util/PasswordGenerator.kt
@@ -35,73 +35,35 @@ object PasswordGenerator {
         val random = Random(System.nanoTime())
         val password = StringBuilder()
 
-        for (i in 0 until length) {
+       for(i in 0 until minNumNumeric){
+           val rIndex = random.nextInt(this.numeric.length)
+           password.append(this.numeric[rIndex])
+       }
+       for(i in 0 until minNumSpecial){
+           val rIndex = random.nextInt(this.special.length)
+           password.append(this.special[rIndex])
+       }
+        for (i in 0 until (length-minNumNumeric-minNumSpecial)) {
             val rIndex = random.nextInt(dictionary.length)
             password.append(dictionary[rIndex])
         }
-      /*val s = password.toString()
-       val array= s.toCharArray()
 
-       array.shuffle(Random(System.nanoTime()))
-
-       return array.toString()
-      //(ContentValues.TAG, password.toString(), shuffle.toString())*/
-       val array= password.toString().toCharArray()
-       array.shuffle()
-       Log.v("${password.toString()}:  ", array.toString())
-        return password.toString()//verify(minNumNumeric,minNumSpecial,password.toString())
+        return verify(password.toString())
 
     }
 
-    private fun verify(minNumNumeric: Int, minNumSpecial: Int, generatedPassword: String):String{
-        val password = StringBuilder()
-        for (i in 0 until generatedPassword.length){
-            password.append(generatedPassword[i])
-        }
+    private fun verify(generatedPassword: String):String{
+
+       val list = generatedPassword.toMutableList()
         val random = Random(System.nanoTime())
-        val dictionary=this.capitalLetter+this.lowerCaseLetters
-        var numNumCheck = false
-        var numSpeCheck = false
-        val countNum = this.numeric.count{ generatedPassword.contains(it) }
-        val countSpecial = this.numeric.count{ generatedPassword.contains(it) }
-        var i =0
-        var charList : MutableList<Int> = mutableListOf()
-        for(ch:Char in generatedPassword){
-            if (dictionary.contains(ch)){
-                charList.add(i)
-            }
-            i++
-        }
-        if(countNum>=minNumNumeric){
-            numNumCheck = true
-        }
-        if(countSpecial>=minNumSpecial){
-            numSpeCheck = true
-        }
-        if (numNumCheck && numSpeCheck){
-            return generatedPassword
-        }
-        if (!numNumCheck){
-            val lessNum = minNumNumeric-countNum
-            for (i in 1..lessNum){
-                val rIndex = random.nextInt(charList.size)
-                val nIndex = random.nextInt(this.numeric.length)
-                password[rIndex]=this.numeric[nIndex]
-                charList.removeAt(rIndex)
-            }
+        val password = StringBuilder()
 
+        for (i in 0 until list.size) {
+            val rIndex = random.nextInt(list.size)
+            password.append(list[rIndex])
+            list.removeAt(rIndex);
         }
-        if (!numSpeCheck){
-            val lessNum = minNumSpecial-countSpecial
-            for (i in 1..lessNum){
-                val rIndex = random.nextInt(charList.size)
-                val nIndex = random.nextInt(this.special.length)
-                password[rIndex]=this.special[nIndex]
-                charList.removeAt(rIndex)
-            }
-        }
-        return password.toString()
-
+       return password.toString()
     }
 
 
